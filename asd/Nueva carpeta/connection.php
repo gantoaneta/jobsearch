42cddd1db67b41e240794d30648335fd88bd3189
maria gal <maria gal@Antoaneta>
2018-12-16 00:07:14 +0200

    update

--- asd/Nueva carpeta/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-function connection($host = "localhost", $username = "urbicaro_urb", $pass = "URBICA.14", $db_name = "urbicaro_urb"){
-    $conn = mysqli_connect($host, $username, $pass, $db_name);
-    return $conn;
-}
